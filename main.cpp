95cd9d9f0d7b08cfba25536d53186b2abc567f36
Andrew Scott <andrewscott@email.arizona.edu>
2018-10-25 20:53:46 -0700

    first init

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ECE474_Assignment2
+//
+//  Created by AJ Scott on 10/25/18.
+//  Copyright © 2018 AJ Scott. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
